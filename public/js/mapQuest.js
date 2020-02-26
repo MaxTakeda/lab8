f64ab00e1353821e289eab31f6e2aff476529165
Maxwell Takeda <mjtakeda@ucsd.edu>
2020-02-26 09:41:25 -0800

    Added API

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'quN9R9apEoxFTqOpVZulTaX3KtSAJJHc';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.8788033, -117.2381008],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+L.marker([32.8788033, -117.2381008]).addTo(map);
 }
